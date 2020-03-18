7af59b5aa7d91c5cbe2920979d4e93bddaec7636
kandrades <k.andrades@hotmail.com.br>
2020-03-18 13:28:54 -0300

    login finalizado, atenticação para o header também

--- combustivel/src/App.js
+++ combustivel/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import Login from './pages/login-page/login-page.component';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-
+import Header from './components/Header/header.component';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/login-page/login-page.component';
+
 
 function App() {
   return (
+    <>
     <BrowserRouter>
+      <Header/>
       <Switch>
         <Route exact path="/login" component={Login} />
       </Switch>
     </BrowserRouter>
+  </>
   );
 }
 
--- combustivel/src/pages/login-page/login-page.component.jsx
+++ combustivel/src/pages/login-page/login-page.component.jsx
@@ -1,50 +1,115 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 
 import FormInput from '../../components/form-input/form-input.componets';
 import CustomButton from '../../components/custom-button/custom-button.component';
 
 import FormGroup from 'react-bootstrap/FormGroup'
+import Alert from 'react-bootstrap/Alert'
 import './login-page.styles.scss';
+import axios from "axios";
 
 class LoginPage extends Component {
     constructor(props) {
         super(props)
 
+        if (sessionStorage.getItem('user'))
+            this.props.history.push('/');
+
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            user: "",
+            errorMessage: ""
         }
+    }
+
+    clearMessage = () => {
+        this.setState({ errorMessage: "" })
+    }
+
+    emptyInputErrorMessage = () => {
+        this.setState({ errorMessage: "Os campos de email e senha estão vazios" })
+    }
+
+    credentialsErrorMessage = () => {
+        this.setState({ errorMessage: "Email ou senha estão incorrentos" })
     }
 
     handleChange = e => {
         const { name, value } = e.target;
-
-        this.setState({ [name] : value })
+        this.setState({ [name]: value });
     }
 
-    handleSignIn = e => {
+    handleSignIn = async e => {
         e.preventDefault();
-        console.log(e);
+
+        const { email, password } = this.state;
+
+
+        this.clearMessage();
+        if (!email || !password) {
+            this.emptyInputErrorMessage()
+        } else {
+            try {
+
+                const { data: response } = await axios.get('http://localhost:8080/login', {
+                    params: {
+                      email: `${email}`
+                    }
+                  })
+
+                if (response.password === password) {
+                    this.props.history.push("/");
+                    sessionStorage.setItem("user", JSON.stringify(response.name));
+                    window.location.reload();
+                }
+                else
+                    this.credentialsErrorMessage()
+            } catch (err) {
+                this.credentialsErrorMessage()
+                    ;
+            }
+        }
     }
 
     render() {
         return (
-            <div className="container">
+            <div className="container mt-4">
                 <div className="row d-flex justify-content-center">
                     <div className="sign-in">
-                        <form method="get">
-                        <FormGroup>
-                        <div className="text-container" >
-                            <h2>Login do cliente</h2>
-                            <span className="span-signin" >Entre com email e senha</span>
-                        </div>
-                        <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label='email' required />
-                        <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label='senha'required />
-                        <CustomButton onClick={this.handleSignIn} >Continuar</CustomButton>
-                        </FormGroup>
-                        <span>Não tem conta ? <Link to='/cadastro'>cadastre-se</Link></span>
+                        <form method="get" onSubmit={this.handleSignIn}>
+                            <FormGroup>
+                                <div className="text-container" >
+                                    <h2>Login do cliente</h2>
+                                    <span className="span-signin" >Entre com email e senha</span>
+                                </div>
+                                {this.state.errorMessage ? (<Alert className="m-4" variant='danger'>{this.state.errorMessage}</Alert>) : ""}
+                                <FormInput
+                                    name="email"
+                                    type="email"
+                                    value={this.state.email}
+                                    handleChange={this.handleChange}
+                                    label='email'
+                                    required />
+
+                                <FormInput name="password"
+                                    type="password"
+                                    value={this.state.password}
+                                    handleChange={this.handleChange}
+                                    label='senha'
+                                    required />
+                                <CustomButton
+                                    type="submit"
+                                    onClick={this.handleSignIn} >
+                                    Continuar
+                            </CustomButton>
+
+                            </FormGroup>
+
+                            <span>Não tem conta ? <Link to='/cadastro'>cadastre-se</Link></span>
+
                         </form>
                     </div>
                 </div>
@@ -53,4 +118,4 @@ class LoginPage extends Component {
     }
 }
 
-export default LoginPage;
+export default withRouter(LoginPage);
