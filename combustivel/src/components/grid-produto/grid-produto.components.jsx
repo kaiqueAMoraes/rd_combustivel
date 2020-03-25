6315399995862df2faa1961ab785ccd69fd03fea
Treinamento <Treinamento@RD723929.raiadrogasil.com>
2020-03-25 14:39:01 -0300

    atualização component grid-produto

--- combustivel/src/components/grid-produto/grid-produto.components.jsx
+++ combustivel/src/components/grid-produto/grid-produto.components.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import '../grid-produto/grid-produto.styles.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,39 +9,64 @@ import {
 } from 'reactstrap';
 
 
-const Produto = (props) => {
-    console.log(props);
+class Product extends Component {
 
-    return (
-        <Container className="telaProduto">
-            <Row className="d-flex justify-content-center">
-                <div className='card'>
-                    <Row className="card-compra" >
-                        <Col md="4" lg="4" className="icone-compra-produto">
-                            <FontAwesomeIcon icon={faGasPump} className="iconGas fas fa-gas-pump card-icon-test" size="5x" />
-                        </Col>
-                        <Col md="8" lg="8" className="produto">
-                            <div className="card-body">
-                                <Col md="4" lg="4">
-                                    <h1 className="card-title nome-produto">Nome do Produto</h1>
-                                    <p className="card-text descricao-produto">This is a wider card with supporting text below
-                                    as a natural lead-in to
-                                    additional content. This content is a little bit longer.
-                                            </p>
-                                </Col>
-                                <Col md="4" lg="4" className="adicionar-produto">
-                                    <h1 className="card-text preco-produto">R$98,99</h1>
-                                    <div>por litro</div>
-                                    <Button type="submit" color="success" className="btn-addCarrinho">Adicionar ao carrinho</Button>
-                                    <br />
-                                </Col>
-                            </div>
-                        </Col>
-                    </Row>
-                </div>
-            </Row>
-        </Container>
-    )
+constructor(props){
+    super(props);
+    state = {
+        
+    }
+
+    getProduct = async () => {
+        const DataProduct = await axios("http://localhost:8080/find-product/" + id);
+
+        this.setState({
+            id: DataProduct.idProduct,
+            name: DataProduct.name,
+            description: DataProduct.description,
+            image: DataProduct.image,
+            price: DataProduct.price,
+            quantStock: DataProduct.quantStock,
+            idCategory: DataProduct.idCategory 
+        })
+    }
 }
 
-export default Produto;
+    // const Produto = (props) => {
+    //     console.log(props);
+
+    render() {
+        return (
+            <Container className="telaProduto">
+                <Row className="d-flex justify-content-center">
+                    <div className='card'>
+                        <Row className="card-compra" >
+                            <Col md="4" lg="4" className="icone-compra-produto">
+                                <FontAwesomeIcon icon={faGasPump} className="iconGas fas fa-gas-pump card-icon-test" size="5x" />
+                            </Col>
+                            <Col md="8" lg="8" className="produto">
+                                <div className="card-body">
+                                    <Col md="4" lg="4">
+                                        <h1 className="card-title nome-produto">{this.state.name}</h1>
+                                        <p className="card-text descricao-produto">This is a wider card with supporting text below
+                                        as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                            </p>
+                                    </Col>
+                                    <Col md="4" lg="4" className="adicionar-produto">
+                                        <h1 className="card-text preco-produto">R$98,99</h1>
+                                        <div>por litro</div>
+                                        <Button type="submit" color="success" className="btn-addCarrinho">Adicionar ao carrinho</Button>
+                                        <br />
+                                    </Col>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Product;
