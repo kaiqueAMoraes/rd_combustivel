12f285ed3952351c2cad246f398fe3f7860b0401
kaique andrades <k.andrades@hotmail.com.br>
2020-03-25 17:56:35 -0300

    cpf change

--- combustivel/src/pages/cadastro-page/cadastro.component.jsx
+++ combustivel/src/pages/cadastro-page/cadastro.component.jsx
@@ -95,7 +95,18 @@ class CadastroPage extends Component {
         var Soma;
         var Resto;
         Soma = 0;
-        if (strCPF === "00000000000") return false;
+        if (
+            strCPF === "00000000000" || 
+            strCPF === "11111111111" ||
+            strCPF === "22222222222" ||
+            strCPF === "33333333333" ||
+            strCPF === "44444444444" ||
+            strCPF === "55555555555" ||
+            strCPF === "66666666666" ||
+            strCPF === "77777777777" ||
+            strCPF === "88888888888" ||
+            strCPF === "99999999999" 
+        ) return false;
 
         for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
         Resto = (Soma * 10) % 11;
@@ -205,8 +216,6 @@ class CadastroPage extends Component {
                 } finally {
 
                 }
-
-
             }
             else throw new Error("cpf invalido");
 
@@ -216,12 +225,6 @@ class CadastroPage extends Component {
         }
 
     }
-
-
-
-
-
-
 
     render() {
         return (
