f0761a736acd6264de047c0e2586ee4e5ad441ae
Hugo Goes <hugo.henkel@hotmail.com>
2020-03-25 14:16:14 -0300

    Classe configuração agora permite put e delete

--- Backend/src/main/java/br/com/rd/Backend/configuration/MyConfiguration.java
+++ Backend/src/main/java/br/com/rd/Backend/configuration/MyConfiguration.java
@@ -13,7 +13,9 @@ public class MyConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
             }
         };
     }
