363f56043c1e6e3b27ef650f8b81ed14edbd5b17
kaique andrades <k.andrades@hotmail.com.br>
2020-03-27 16:40:00 -0300

    adicionado animações no cad-prod

--- combustivel/src/pages/home/home-page.jsx
+++ combustivel/src/pages/home/home-page.jsx
@@ -1,42 +1,49 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 
-// import { Container } from './styles';
+import './home.styles.scss';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
-            products : []
+            products: []
         }
     }
 
     componentDidMount = async () => {
         await axios.get("http://localhost:8080/find-all-products")
-        .then(response => {
-        
-            this.setState({products : response.data},
-                () => {
-                    console.log(this.state.products)
-                })
-        }).catch(error => {
-            console.log(error)
-        });
+            .then(response => {
+
+                this.setState({ products: response.data },
+                    () => {
+                        console.log(this.state.products)
+                    })
+            }).catch(error => {
+                console.log(error)
+            });
     }
-    
-    
+
+
     render() {
-        const {products} = this.state;
-        const {description, idCategory, image, idProduct, name, quantStock, price} = this.state.products;
+        const { products } = this.state;
+        const { description, idCategory, image, idProduct, name, quantStock, price } = this.state.products;
         const MyComponents = { // cria componetização dinamica na pagina por um objeto, assim não é necessario criar callbacks no jsx
             Products: function showProducts() {
                 return products.map(elm => {
                     return (
-                        <div key={elm.idProduct} className="prod-card">    
-                            <img src={elm.image} alt="image prod"/>
-                            <h5>{elm.name}</h5>
-                            <h5>{elm.price}</h5>
+                        <div key={elm.idProduct} className="prod-card">
+                            
+                                <div className="prod-image">
+                                    <img src={elm.image} alt="image prod" />
+                                </div>
+                                <div className="prod-content">
+                                    <h5 className="prod-price">R${elm.price}</h5>
+                                    <h5 className="prod-title">{elm.name}</h5>
+                                </div>
+                         
                         </div>
                     )
                 })
@@ -45,7 +52,7 @@ export default class Home extends Component {
 
 
         //console.log(this.state.products)
-        return (    
+        return (
             <MyComponents.Products></MyComponents.Products>
         )
     }
