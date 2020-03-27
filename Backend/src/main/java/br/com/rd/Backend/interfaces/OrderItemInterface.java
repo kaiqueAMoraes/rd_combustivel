438f7211b43f3a421c11b82d449f7b17e28e0d4e
Hugo Goes <hugo.henkel@hotmail.com>
2020-03-27 13:28:32 -0300

    Criada a interface para OrderItem

---
+++ Backend/src/main/java/br/com/rd/Backend/interfaces/OrderItemInterface.java
@@ -0,0 +1,17 @@
+package br.com.rd.Backend.interfaces;
+
+import br.com.rd.Backend.DTOs.OrderItemDTO;
+import br.com.rd.Backend.models.Order;
+import br.com.rd.Backend.models.OrderItem;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface OrderItemInterface {
+
+    ResponseEntity saveOrderItem(OrderItemDTO orderItemDTODTO);
+
+    ResponseEntity findOrderItemByIdOrder(Order order);
+
+    ResponseEntity<List<OrderItem>> findAllOrderItems();
+}
