b1f4caf743e33c68dfd82b8be05c4f26a57548e2
Hugo Goes <hugo.henkel@hotmail.com>
2020-03-27 15:25:44 -0300

    Adicionado o método findAll

--- Backend/src/main/java/br/com/rd/Backend/controllers/OrderItemController.java
+++ Backend/src/main/java/br/com/rd/Backend/controllers/OrderItemController.java
@@ -4,6 +4,7 @@ import br.com.rd.Backend.DTOs.OrderItemDTO;
 import br.com.rd.Backend.services.OrderItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,12 @@ public class OrderItemController {
     OrderItemService orderItemService;
 
     @PostMapping("/create-orderitem")
-    public ResponseEntity createOrder(@RequestBody OrderItemDTO orderItemDTO) {
+    public ResponseEntity createOrderItem(@RequestBody OrderItemDTO orderItemDTO) {
         return orderItemService.saveOrderItem(orderItemDTO);
+    }
+
+    @GetMapping("/findall-orderitems")
+    public ResponseEntity findAllOrderItems() {
+        return orderItemService.findAllOrderItems();
     }
 }
