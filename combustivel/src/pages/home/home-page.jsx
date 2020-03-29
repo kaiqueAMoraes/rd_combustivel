e36a1639b4cbf052584f6d015a2f69f07729738e
kaique andrades <k.andrades@hotmail.com.br>
2020-03-29 04:23:16 -0300

    carousel, div scroller quase prontos

--- combustivel/src/pages/home/home-page.jsx
+++ combustivel/src/pages/home/home-page.jsx
@@ -14,7 +14,11 @@ class Home extends Component {
 
         this.state = {
             products: [],
-            left: 100
+            newer: [],
+            left: 100,
+            right: 0,
+            showbtnbefore: false,
+            showbtnnext : true
         }
     }
 
@@ -24,25 +28,37 @@ class Home extends Component {
                 console.log(response.data[0].idProduct)
                 this.setState({ products: response.data },
                     () => {
-                        console.log(this.state.products)
+                        const n = [];
+                        for (let i = 0; i < 15; i++) {
+                            const element = this.state.products[i];
+                            n.push(element);
+                        }
+                        this.setState({ newer: n })
                     })
             }).catch(error => {
                 console.log(error)
             });
+
+
     }
 
+    // HANDLES DO CAROUSEL COMEÇAM AQUI CAROUSEL COMEÇA AQUI
     handleCarouselLeft = () => {
+        // LEFT É O STATE "FINAL=0/INITIAL=400" QUE EMPURRA AS "DIVS"
         if (this.state.left !== 0) {
-            this.setState({ left: this.state.left -= 100 },
+            // A CONTA FOI FEITA USANDO VW(viewport width) POR ISSO TEM QUES ETAR SEMPRE INCREMENTANDO OU DECREMENTANDO 100
+            this.setState({ left: this.state.left -= 100 }, 
                 () => { console.log(this.state.left) })
-            }
-            if (this.state.left === 0) {
-                this.setState({ left: 400 }
-                )
-            }
-        
+        }
+        // CHEGANDO A ZERO ELE RESETA AO INITIAL STATE
+        if (this.state.left === 0) {
+            this.setState({ left: 400 }
+            )
+        }
+
     }
 
+    // scroll para a direita do carousel ---> ///
     handleCarouselRight = () => {
         this.setState({ left: this.state.left += 100 },
             () => { console.log(this.state.left) })
@@ -52,16 +68,92 @@ class Home extends Component {
             )
         }
     }
+    // scroll para a direita do carousel ---> termina aqui ///
+
+
+
+    // SCROLL dos produtos começa aqui ///
+
+    // scroll para a esquerda <--- ///
+
+    handleScrollProducts = () => {
+        let counter = 0; //COUNTER COMEÇA EM ZERO- ELE É O HOLDER DO INTERVAL
+        let count = 936 / 11; // 936 É O VALOR DO PRIMEIRO SCROLL DOS 5 ITENS, É DIVIDIDO EM 11 POIS O COUNT PARA DEPOIS DE 10
+        let limit = 936 * 2 - 20; // não mudar esse desgraçado aqui // CONTA PARA DETERMINAR QUANDO MOSTRAR O BOTÃO
+
+        // ATRIBUINDO UMA FUNÇÃO QUE VAI FUNCIONAR COMO INTERVAL, PARA TER CONTROLE DE ITERAÇÃO
+        let scrollControler =  () => {
+            if (counter <= 10) { //NUMERO DE VEZES PARA ITERAR E FAZER A ANIMAÇÃO DE SCROLL USANDO CLASSES
+                this.setState({ right: this.state.right -= count }, // DIVISÃO DO TOTAL / 11 DA TELA PARA INCREMENTAR UMA VOLTA COMPLETA DOS ITENS
+                    () => {
+                        console.log(this.state.right) //DEBUG TODO: APAGAR DEPOIS ISSO AQUI 
+                        counter++; //INCREMENTA VALOR
+                    });
+                    //DEBUG TODO: APAGAR DEPOIS ISSO AQUI 
+                    console.log('counter : ' + counter + "// count : " + count + ` // limit is ${limit} `)
+                } else {
+                    //LIMPA O STATE E PREVINI DE ACONTECER APÓS AS 10 ITERAÇÕES = 936px/100vw
+                    clearInterval(scrollAction);
+                }
+            }
+            let scrollAction = setInterval(scrollControler, 10);
+
+            //CHECA VALOR TOTAL, SE FOR A ULTIMA ITERAÇÃO e
+            this.setState({showbtnnext : true})
+
+            if(this.state.right >= limit ){
+                this.setState({showbtnbefore : false})
+            }
+    }
+
+
+    // scroll para a direita ---> ///
+    handleScrollNextProducts = () => {
+        let counter = 0; //COUNTER COMEÇA EM ZERO- ELE É O HOLDER DO INTERVAL
+        let count = 936 / 11; // 936 É O VALOR DO PRIMEIRO SCROLL DOS 5 ITENS, É DIVIDIDO EM 11 POIS O COUNT PARA DEPOIS DE 10
+        let limit = 936 * 2 - 20; // não mudar esse desgraçado aqui // CONTA PARA DETERMINAR QUANDO MOSTRAR O BOTÃO
+
+        // ATRIBUINDO UMA FUNÇÃO QUE VAI FUNCIONAR COMO INTERVAL, PARA TER CONTROLE DE ITERAÇÃO
+        let scrollControler =  () => {
+            if (counter <= 10) { //NUMERO DE VEZES PARA ITERAR E FAZER A ANIMAÇÃO DE SCROLL USANDO CLASSES
+                this.setState({ right: this.state.right += count }, // DIVISÃO DO TOTAL / 11 DA TELA PARA INCREMENTAR UMA VOLTA COMPLETA DOS ITENS
+                    () => {
+                        console.log(this.state.right) //DEBUG TODO: APAGAR DEPOIS ISSO AQUI 
+                        counter++; //INCREMENTA VALOR
+                    });
+                    //DEBUG TODO: APAGAR DEPOIS ISSO AQUI 
+                    console.log('counter : ' + counter + "// count : " + count + ` // limit is ${limit} `)
+                } else {
+                    //LIMPA O STATE E PREVINI DE ACONTECER APÓS AS 10 ITERAÇÕES = 936px/100vw
+                    clearInterval(scrollAction);
+                }
+            }
+            let scrollAction = setInterval(scrollControler, 10);
+            this.setState({showbtnbefore : true})
+            //CHECA VALOR TOTAL, SE FOR A ULTIMA ITERAÇÃO e
+            if(this.state.right >= limit ){
+                this.setState({showbtnnext : false})
+            }
+    }
+    // scroll para a direita ---> termina aqui ///
+
 
     render() {
-        const { products } = this.state;
+        const { products, newer, showbtnnext, showbtnbefore } = this.state;
         const left = {
-            right: `${this.state.left}vw`
+            right: `${this.state.left}vw`,
         }
 
+        const right = {
+            transition: "all 1s ease-out",
+            right: `${this.state.right}px`
+        }
+
+        const handleScrollProducts = this.handleScrollProducts;
+        const handleScrollNextProducts = this.handleScrollNextProducts
 
         const MyComponents = { // cria componetização dinamica na pagina por um objeto, assim não é necessario criar callbacks no jsx
-            Discount: function showAddresses() {
+            Discount: function showDiscountItems() {
                 return (
                     <Row className="justify-content-around">
                         <div className="discount-container">
@@ -100,6 +192,33 @@ class Home extends Component {
                         </div>
                     </Row>
                 )
+            },
+            scrollProducts: function showNewerProducts() {
+                return (
+                    <Row className="hold">
+                        <div className="scroll-products-container-control">
+                            <div style={right} className="scroll-products-container">
+                                {
+                                    newer.map(elm => {
+                                        return (
+                                            <CardProd
+                                                key={elm.idProduct}
+                                                idprod={elm.idProduct}
+                                                name={elm.name}
+                                                image={elm.image}
+                                                price={elm.price}
+                                                description={elm.description}
+                                            />
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                        { showbtnbefore ?  (<button className="btn-before" onClick={handleScrollProducts}>before</button>) : "" }
+                        { showbtnnext ?  (<button className="btn-next" onClick={handleScrollNextProducts}>next</button>) : "" }
+                        
+                    </Row>
+                )
             }
         }
 
@@ -107,25 +226,30 @@ class Home extends Component {
 
         return (
             <>
-                {/* CAROUSEL ===> TO COMPONENT */}
+                {/* CAROUSEL ===> TO CREATE COMPONENT */}
                 <div className="carousel-controller">
                     <button className="left" onClick={this.handleCarouselLeft}>left</button>
-                    <div className="carousel-container"  >
-                        <div className="carousel carousel-a" style={left}>1</div>
-                        <div className="carousel carousel-b" style={left}>2</div>
-                        <div className="carousel carousel-c" style={left}>3</div>
-                        <div className="carousel carousel-d" style={left}>4</div>
-                        <div className="carousel carousel-e" style={left}>5</div>
+                    <div className="carousel-container">
+                        <div className="carousel carousel-a" style={left}><span>0</span></div>
+                        <div className="carousel carousel-b" style={left}><span>1</span></div>
+                        <div className="carousel carousel-c" style={left}><span>2</span></div>
+                        <div className="carousel carousel-d" style={left}><span>3</span></div>
+                        <div className="carousel carousel-e" style={left}><span>4</span></div>
                     </div>
                     <button className="right" onClick={this.handleCarouselRight}>right</button>
                 </div>
-                {/* CAROUSEL ===> TO COMPONENT */}
+                {/* CAROUSEL ===> TO CREATE COMPONENT */}
 
                 <Container className="container-fluid">
 
-                    {/* DISCOUNTS ===> TO COMPONENT */}
+                    {/* DISCOUNTS ===> TO CREATE COMPONENT */}
                     <MyComponents.Discount></MyComponents.Discount>
-                    {/* DISCOUNTS ===> TO COMPONENT */}
+                    {/* DISCOUNTS ===> TO CREATE COMPONENT */}
+
+                    {/* NEWER PRODUCTS ===> TO CREATE COMPONENT */}
+                    <h2>Veja os mais vendidos</h2>
+                    <MyComponents.scrollProducts></MyComponents.scrollProducts>
+                    {/* NEWER PRODUCTS ===> TO CREATE COMPONENT */}
 
                     <Row className="justify-content-between">
                         <div className="full" ></div>
