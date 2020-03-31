0849a03951355f38ff6c478d83b7ae887fd17b04
Treinamento <Treinamento@RD723942.raiadrogasil.com>
2020-03-31 00:58:01 -0300

    Adicionado o header a página main

--- admmodule/src/pages/Main/index.js
+++ admmodule/src/pages/Main/index.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { TiPowerOutline } from 'react-icons/ti';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
 
 export default function Main() {
-  return (
-    <h1>Works!</h1>
-  );
+
+    const history = useHistory();
+
+    function handleLogout(evt) {
+        evt.preventDefault();
+
+        history.push("/");
+    }
+
+    return (
+        <div>
+            <div className="header-container">
+                <img src={logoImg} width="200" alt="Origin Combustíveis" />
+                <Link className="link" to="/">Pedidos</Link>
+                <Link className="link" to="/">Cadastrar Produto</Link>
+                <Link className="link" to="/">Clientes</Link>
+                <button onClick={handleLogout} type="button">
+                    <TiPowerOutline size={40} color="#E02041" />
+                </button>
+            </div>
+
+            <div className="main-container">
+
+            </div>
+        </div>
+    );
 }
