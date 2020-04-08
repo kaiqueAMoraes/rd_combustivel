89c6e108c05fd383a2c51328bd8afe74812fed1a
Hugo Goes <hugo.henkel@hotmail.com>
2020-04-08 17:41:35 -0300

    Removido import com erro

--- Backend/src/main/java/br/com/rd/Backend/controllers/UserController.java
+++ Backend/src/main/java/br/com/rd/Backend/controllers/UserController.java
@@ -4,12 +4,10 @@ import br.com.rd.Backend.DTOs.UserDTO;
 import br.com.rd.Backend.models.Login;
 import br.com.rd.Backend.models.User;
 import br.com.rd.Backend.services.UserService;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
