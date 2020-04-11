4cc485615a1b2371c8e0e5b76a1fc7719c72d7b7
Hugo Goes <hugo.henkel@hotmail.com>
2020-04-10 21:50:15 -0300

    adicionado método configure

--- Backend/src/main/java/br/com/rd/Backend/configuration/WebSecurityConfig.java
+++ Backend/src/main/java/br/com/rd/Backend/configuration/WebSecurityConfig.java
@@ -3,11 +3,13 @@ package br.com.rd.Backend.configuration;
 import br.com.rd.Backend.auth.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -32,10 +34,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         UsernamePasswordAuthenticationFilter.class);
     }
 
-//    @Autowired
-//    public void createUser(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(customUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-//    }
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //create a default account
+        auth.inMemoryAuthentication()
+        .withUser("admin")
+        .password("admin")
+        .roles("ADMIN");
+    }
 
 
 }
