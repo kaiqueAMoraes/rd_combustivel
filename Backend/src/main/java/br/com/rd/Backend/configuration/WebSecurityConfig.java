2b1577f8d87ff5becef4636c5212d890eb73a710
Hugo Goes <hugo.henkel@hotmail.com>
2020-04-10 21:44:23 -0300

    Alteracao na estrutura de segurança do WebSecurity

--- Backend/src/main/java/br/com/rd/Backend/configuration/WebSecurityConfig.java
+++ Backend/src/main/java/br/com/rd/Backend/configuration/WebSecurityConfig.java
@@ -2,32 +2,34 @@ package br.com.rd.Backend.configuration;
 
 import br.com.rd.Backend.auth.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     CustomUserDetailsService customUserDetailsService;
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                //Pedir para que todas as rotas sejam autorizadas
-                .authorizeRequests().antMatchers("/create-user", "/find-user/{id}", "/find-user-email/{email}")
-                .permitAll()
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
 
+                //Filter login requests
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
 
-                //Tirar sessão da aplicação
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                .and().httpBasic()
-                //Desabilitar token
-                .and().csrf().disable();
+                //Filter other requests to verify JWT presence on header
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
     }
 
 //    @Autowired
