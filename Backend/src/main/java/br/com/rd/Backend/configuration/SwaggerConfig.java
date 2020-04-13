f3bd7ca0e88dca87d6ff338bc9c52aa642146ca3
Hugo Goes <hugo.henkel@hotmail.com>
2020-04-13 17:26:05 -0300

    Adicionado informacoes ao swagger e liberação dos endpoints

--- Backend/src/main/java/br/com/rd/Backend/configuration/SwaggerConfig.java
+++ Backend/src/main/java/br/com/rd/Backend/configuration/SwaggerConfig.java
@@ -3,10 +3,13 @@ package br.com.rd.Backend.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -19,18 +22,23 @@ import java.util.List;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    static final String BASE_PACKAGE = "br.com.rd.Backend.controllers";
+    static final String URLS = "/**";
+
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.ant(URLS))
                 .build()
                 .useDefaultResponseMessages(false)
                 .globalResponseMessage(RequestMethod.GET, responseMessageForGET())
                 .globalResponseMessage(RequestMethod.POST, responseMessageForPOST())
                 .globalResponseMessage(RequestMethod.DELETE, responseMessageForDELETE())
-                .globalResponseMessage(RequestMethod.PUT, responseMessageForPUT());
+                .globalResponseMessage(RequestMethod.PUT, responseMessageForPUT())
+                .apiInfo(apiInfo());
     }
 
     private List<ResponseMessage> responseMessageForGET()
@@ -71,5 +79,16 @@ public class SwaggerConfig {
                     .message("400 Bad Request")
                     .build());
         }};
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Origin Combustíveis")
+                .description("Documentação da API")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                        .contact(new Contact("Origin Combustíveis", "https://github.com/kaiqueAMoraes/rd_combustivel", "origin.combustivel@gmail.com"))
+                        .build();
     }
 }
