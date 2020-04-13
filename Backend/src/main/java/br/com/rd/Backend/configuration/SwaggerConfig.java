9ee3aa5240200131ee737d411958fcafe57788a3
Hugo Goes <hugo.henkel@hotmail.com>
2020-04-13 15:07:52 -0300

    Adicionada lista de errors para o CRUD

--- Backend/src/main/java/br/com/rd/Backend/configuration/SwaggerConfig.java
+++ Backend/src/main/java/br/com/rd/Backend/configuration/SwaggerConfig.java
@@ -2,11 +2,18 @@ package br.com.rd.Backend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +25,51 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, responseMessageForGET())
+                .globalResponseMessage(RequestMethod.POST, responseMessageForPOST())
+                .globalResponseMessage(RequestMethod.DELETE, responseMessageForDELETE())
+                .globalResponseMessage(RequestMethod.PUT, responseMessageForPUT());
+    }
+
+    private List<ResponseMessage> responseMessageForGET()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(400)
+                    .message("400 Bad Request")
+                    .build());
+        }};
+    }
+
+    private List<ResponseMessage> responseMessageForPOST()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(400)
+                    .message("400 Bad Request")
+                    .build());
+        }};
+    }
+
+    private List<ResponseMessage> responseMessageForDELETE()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(400)
+                    .message("400 Bad Request")
+                    .build());
+        }};
+    }
+
+    private List<ResponseMessage> responseMessageForPUT()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(400)
+                    .message("400 Bad Request")
+                    .build());
+        }};
     }
 }
