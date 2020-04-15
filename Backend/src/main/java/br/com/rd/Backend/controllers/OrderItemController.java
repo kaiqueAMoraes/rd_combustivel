c67649d97d6cfd9d582192aea983b62cbd0b4183
Aryana Agustavo <aryana_agustavo@hotmail.com>
2020-04-15 12:52:11 -0300

    implementado método top selling - testando

--- Backend/src/main/java/br/com/rd/Backend/controllers/OrderItemController.java
+++ Backend/src/main/java/br/com/rd/Backend/controllers/OrderItemController.java
@@ -2,10 +2,13 @@ package br.com.rd.Backend.controllers;
 
 import br.com.rd.Backend.DTOs.OrderItemDTO;
 import br.com.rd.Backend.models.Order;
+import br.com.rd.Backend.models.Product;
 import br.com.rd.Backend.services.OrderItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orderitem")
@@ -17,5 +20,10 @@ public class OrderItemController {
     @GetMapping(value = "/findall", produces = "application/json")
     public ResponseEntity findAll() {
         return orderItemService.findAll();
+    }
+
+    @GetMapping(value = "/top-selling", produces = "application/json")
+    public ResponseEntity<List<Product>> topSelling() {
+        return orderItemService.topSelling();
     }
 }
--- Backend/src/main/java/br/com/rd/Backend/interfaces/OrderItemInterface.java
+++ Backend/src/main/java/br/com/rd/Backend/interfaces/OrderItemInterface.java
@@ -3,12 +3,17 @@ package br.com.rd.Backend.interfaces;
 import br.com.rd.Backend.DTOs.OrderItemDTO;
 import br.com.rd.Backend.models.Order;
 import br.com.rd.Backend.models.OrderItem;
+import br.com.rd.Backend.models.Product;
 import org.springframework.http.ResponseEntity;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface OrderItemInterface {
 
     ResponseEntity save(List<OrderItem> list);
     ResponseEntity<List<OrderItem>> findAll();
+    ResponseEntity<List<Product>> topSelling() throws SQLException;
+
+
 }
