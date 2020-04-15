24cfd51747ae54b9b560e33a8213301eec87bcb5
kandrades <k.andrades@hotmail.com.br>
2020-04-15 13:31:52 -0300

    iniciado address_card_slider

---
+++ combustivel/src/components/address-card-slide/address-card-slide.component.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
+import Animations from '../../animations/animation_controller';
+
+class AdressSlider extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            fade_animation : {
+                none : ""
+            },
+        }
+    }
+
+    componentDidMount = () => {
+        if (!this.props.hidden) {
+            this.props.hide_cart()
+        }
+    }
+    
+    componentDidUpdate(prevProps) {
+        if (this.props.hidden !== prevProps.hidden) {
+            return (this.handleFadeIn)()
+        }
+    }
+
+    handleFadeIn = () => {
+        const { hidden } = this.props;
+
+        if (!hidden) {
+            return this.setState({
+                fade_animation: Animations.FADE_IN_FROM_LEFT()
+            })
+        }
+    }
+
+
+    render(){
+        return(
+            <h1>ok ok</h1>
+        )
+    }
+}
+
+
+
+const mapStateToProps = createStructuredSelector({
+    hidden: isHidden,
+    addresses: bringAddresses,
+    addressSelected: state.address.addressSelected
+});
+
+export default withRouter(
+    connect(
+        mapStateToProps,
+    )
+        (AdressSlider)
+);
